5bf99b5fc496038af26505ba0da79aec0aada1f8
RikiTikiTavee17 <200taa@mail.ru>
2025-03-19 22:46:16 +0300

    From int64 to int 32

--- go.mod
+++ go.mod
@@ -1,5 +1,19 @@
 module productionSite
 
-go 1.23
+go 1.23.0
 
-require github.com/go-chi/chi v1.5.5
+toolchain go1.23.5
+
+require (
+	github.com/RikiTikiTavee17/productionSite/course/grpc v0.0.0-20250319111312-e30721e5cd8c
+	github.com/go-chi/chi v1.5.5
+	google.golang.org/grpc v1.71.0
+)
+
+require (
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250219182151-9fdb1cabc7b2 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+)
--- main.go
+++ main.go
@@ -7,134 +7,238 @@ import (
 	"github.com/go-chi/chi"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
-	"math/rand"
 	"net/http"
 	"strconv"
-	"sync"
 	"time"
 )
 
+type Dish struct {
+	Id        int64     `json:"id"`
+	Info      *DishInfo `json:"info"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+}
+
+type DishInfo struct {
+	Name        string `json:"name"`
+	Price       int64  `json:"price"`
+	Description string `json:"description"`
+	Composition string `json:"composition"`
+	Author      int64  `json:"author"`
+	PhotoUrl    string `json:"photo_url"`
+}
+
+type Person struct {
+	Id       int64  `json:"id"`
+	Login    string `json:"login"`
+	Password string `json:"password"`
+	Position string `json:"position"`
+}
+
 const (
 	baseUrl       = "localhost:8081"
-	createPostfix = "/notes"
-	getPostfix    = "/notes/{noteId}"
+	createDish    = "/dish"
+	getDish       = "/dish/get/{dishId}"
+	updateDish    = "/dish/update/{dishId}"
+	deleteDish    = "/dish/delete/{dishId}"
+	listDishes    = "/dishes/list"
+	personsCreate = "/persons/create"
+	personsChange = "/persons/change"
+	personsLogIn  = "/persons/login"
 )
 
-type NoteInfo struct {
-	Title    string `json:"title"`
-	Content  string `json:"content"`
-	Author   string `json:"author"`
-	IsPublic bool   `json:"is_public"`
+func convertTimestampToISO8601(ts *timestamppb.Timestamp) string {
+	// Преобразуем timestamp в time.Time
+	t := ts.AsTime()
+	// Преобразуем time.Time в формат ISO 8601
+	return t.Format(time.RFC3339)
 }
 
-type Note struct {
-	ID        int64     `json:"id"`
-	Info      NoteInfo  `json:"info"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+func getGRPCClient() (desc.DishV1Client, *grpc.ClientConn, error) {
+	conn, err := grpc.Dial(baseUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server %v", err)
+	}
+	c := desc.NewDishV1Client(conn)
+	return c, conn, nil
 }
 
-type SyncMap struct {
-	elems map[int64]*Note
-	m     sync.RWMutex
-}
+func createDishHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-var notes = &SyncMap{elems: make(map[int64]*Note)}
-
-func createNoteHandler(w http.ResponseWriter, r *http.Request) {
-	info := &NoteInfo{}
+	info := &DishInfo{}
 	if err := json.NewDecoder(r.Body).Decode(info); err != nil {
-		http.Error(w, "Failed to decode note data", http.StatusBadRequest)
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	now := time.Now()
-
-	note := &Note{
-		ID:        rand.Int63(),
-		Info:      *info,
-		CreatedAt: now,
-		UpdatedAt: now,
-	}
-
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(note); err != nil {
-		http.Error(w, "Failed to encode note data", http.StatusInternalServerError)
+		http.Error(w, "Failed to decode dish data", http.StatusBadRequest)
 		return
 	}
-
-	notes.m.Lock()
-	defer notes.m.Unlock()
-
-	notes.elems[note.ID] = note
-}
-
-func getNoteHandler(w http.ResponseWriter, r *http.Request) {
-	noteId := chi.URLParam(r, "noteId")
-	id, err := parseNoteID(noteId)
+	client, conn, err := getGRPCClient()
 	if err != nil {
-		http.Error(w, "Invalid note id", http.StatusBadRequest)
+		http.Error(w, "Failed to connect to server", http.StatusInternalServerError)
 		return
-	}
-
-	notes.m.RLock()
-	defer notes.m.RUnlock()
-
-	note, ok := notes.elems[id]
-	if !ok {
-		http.Error(w, "Note not found", http.StatusNotFound)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(note); err != nil {
-		http.Error(w, "Failed to encode note data", http.StatusInternalServerError)
-		return
-	}
-}
-
-func parseNoteID(isStr string) (int64, error) {
-	id, err := strconv.ParseInt(isStr, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-	return id, nil
-}
-
-const (
-	address = "localhost:50051"
-	noteId  = 12
-)
-
-func getGRPCClient() (grpc_service.YourServiceClient, *grpc.ClientConn, error) {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("failed to cinnect to server %v", err)
 	}
 	defer conn.Close()
 
-	c := desc.NewNoteV1Client(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	r, err := c.Get(ctx, &desc.GetRequest{Id: noteId})
-	if err != nil {
-		log.Fatalf("failed to get note %v", err)
+	grpcReq := &desc.CreateRequest{
+		Info: &desc.DishInfo{
+			Name:        info.Name,
+			Price:       info.Price,
+			Description: info.Description,
+			Composition: info.Composition,
+			Author:      info.Author,
+			PhotoUrl:    info.PhotoUrl,
+		},
 	}
 
-	log.Printf("Note info:%v", r.GetNote())
+	grpcRes, err := client.Create(context.Background(), grpcReq)
+	if err != nil {
+		http.Error(w, "Failed to create dish", http.StatusInternalServerError)
+		return
+	}
+
+	responce := map[string]interface{}{
+		"id": grpcRes.GetId(),
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(responce); err != nil {
+		http.Error(w, "Failed to encode dish data", http.StatusInternalServerError)
+		return
+	}
+}
+
+func getDishHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := chi.URLParam(r, "dishId")
+	if id == "" {
+		http.Error(w, "Missing dishId", http.StatusBadRequest)
+		return
+	}
+
+	client, conn, err := getGRPCClient()
+	if err != nil {
+		http.Error(w, "Failed to connect to server", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+	newId, _ := strconv.Atoi(id)
+	grpcReq := &desc.GetRequest{
+		Id: int64(newId),
+	}
+	grpcRes, err := client.Get(context.Background(), grpcReq)
+	if err != nil {
+		http.Error(w, "Failed to get dish", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"id":          grpcRes.GetNote().GetId(),
+		"name":        grpcRes.GetNote().GetInfo().GetName(),
+		"price":       grpcRes.GetNote().GetInfo().GetPrice(),
+		"description": grpcRes.GetNote().GetInfo().GetDescription(),
+		"composition": grpcRes.GetNote().GetInfo().GetComposition(),
+		"author":      grpcRes.GetNote().GetInfo().GetAuthor(),
+		"photo_url":   grpcRes.GetNote().GetInfo().GetPhotoUrl(),
+		"created_at":  grpcRes.GetNote().GetCreatedAt().AsTime().Format(time.RFC3339),
+		"updated_at":  grpcRes.GetNote().GetUpdatedAt().AsTime().Format(time.RFC3339),
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode dish data", http.StatusInternalServerError)
+		return
+	}
+}
+
+func listDishesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	client, conn, err := getGRPCClient()
+	if err != nil {
+		http.Error(w, "Failed to connect to server", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	grpcReq := &desc.ListRequest{}
+	grpcRes, err := client.List(context.Background(), grpcReq)
+	if err != nil {
+		http.Error(w, "Failed to list dishes", http.StatusInternalServerError)
+		return
+	}
+	Dishes := make([]Dish, 0)
+	for _, dish := range grpcRes.GetDishes() {
+		Dishes = append(Dishes, Dish{
+			Id:        dish.GetId(),
+			CreatedAt: convertTimestampToISO8601(dish.GetCreatedAt()),
+			UpdatedAt: convertTimestampToISO8601(dish.GetUpdatedAt()),
+			Info: &DishInfo{
+				Name:        dish.GetInfo().GetName(),
+				Price:       dish.GetInfo().GetPrice(),
+				Description: dish.GetInfo().GetDescription(),
+				Composition: dish.GetInfo().GetComposition(),
+				Author:      dish.GetInfo().GetAuthor(),
+				PhotoUrl:    dish.GetInfo().GetPhotoUrl(),
+			},
+		})
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(Dishes); err != nil {
+		http.Error(w, "Failed to encode dishes", http.StatusInternalServerError)
+		return
+	}
+}
+
+func updateDishHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPatch {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	client, conn, err := getGRPCClient()
+	if err != nil {
+		http.Error(w, "Failed to connect to server", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	id := chi.URLParam(r, "dishId")
+	if id == "" {
+		http.Error(w, "Missing dishId", http.StatusBadRequest)
+		return
+	}
+	newId, _ := strconv.Atoi(id)
+	grpcReq := &desc.UpdateRequest{
+		Id: int64(newId),
+	}
 }
 
 func main() {
 	r := chi.NewRouter()
-	r.Post(createPostfix, createNoteHandler)
-	r.Get(getPostfix, getNoteHandler)
+	r.Post(createDish, createDishHandler)
+	r.Get(getDish, getDishHandler)
+	r.Get(listDishes, listDishesHandler)
+	r.Patch(updateDish, updateDishHandler)
+	r.Delete(deleteDish, deleteDishHandler)
+	r.Post(personsCreate, personsCreateHandler)
+	r.Post(personsLogIn, personsLogInHandler)
+	r.Patch(personsChange, personsChangeHandler)
 
 	err := http.ListenAndServe(baseUrl, r)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
